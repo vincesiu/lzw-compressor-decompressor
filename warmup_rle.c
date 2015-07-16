cdf76f036adbee82f78013bc968fb9eb3f27d331
vince <siuv@lnxsrv02.seas.ucla.edu>
2015-07-15 21:12:35 -0700

    Further updated the file compressor so it decompresses

--- warmup_rle.c
+++ warmup_rle.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main (int argc, char *argv[]) {
+void compression(char *file) {
 
-	if (argc != 3 || (*argv[1] != 'd' && *argv[1] != 'c'))
-		printf("Error: this program requires two arguments.\nThe first argument is 'd' for decompressing or 'c' for compressing, and the second argument is the name of the file.\n");
-	
-	printf("%d", strlen(argv[2]));
-	FILE *fp_input = fopen (argv[2], "r");
-	FILE *fp_output = fopen("newfile", "w");
-	int i;
-	//for (i = 0; i < 10; i++) {
-	//printf("%c", fgetc(fp));
-	//}
+	FILE *fp_input = fopen (file, "r");
+	FILE *fp_output = fopen("compressed_file", "w");
+
+
 	char c;
 	char active_char = fgetc(fp_input);
 	char active_char_count = 1;
 
 	while ((c = fgetc(fp_input)) != EOF){
-		if (c != active_char){
-			fprintf(fp_output, "%d%c", active_char_count, active_char);
+		if (c != active_char || active_char_count == 9){
+			if (active_char_count == 1)
+				fprintf(fp_output, "%c", (active_char_count+128));
+			else
+				fprintf(fp_output, "%d%c", active_char_count, active_char);
 			active_char = c;
 			active_char_count = 1;
 		}
@@ -31,8 +29,50 @@ int main (int argc, char *argv[]) {
 	fprintf(fp_output, "%d%c", active_char_count, active_char);
 
 	fclose(fp_input);	
+	fclose(fp_output);
+
+
+}
 
 
 
+void decompression (char *file) {
+
+	FILE *fp_input = fopen (file, "r");
+	FILE *fp_output = fopen("decompressed_file", "w");
+	
+	char c;
+	while ((c = fgetc(fp_input)) != EOF) {
+		if (c < 0) 
+			fprintf(fp_output, "%c", c+=128);		
+		else {
+			int count = c - '0';
+			char active_char = fgetc(fp_input);
+			int idx;
+			for (idx = 0; idx < count; idx++)
+				fprintf(fp_output, "%c", active_char);		
+		}
+			
+	}
+}
+
+
+int main (int argc, char *argv[]) {
+
+	if (argc != 3 || (*argv[1] != 'd' && *argv[1] != 'c'))
+		printf("Error: this program requires two arguments.\nThe first argument is 'd' for decompressing or 'c' for compressing, and the second argument is the name of the file.\n");
+	
+	
+	switch(argv[1][0]) {
+                case('c'):	
+			compression(argv[2]);
+			break;
+		case('d'):
+			decompression(argv[2]);
+			break;
+		default:
+			printf("Invalid argument. Please provide the first argument as 'c' for compressing or 'd' for decompressing");
+
+}
 
 }
